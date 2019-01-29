cae1bbb59716077925e09b540bb72851697011f7
CaroMG <caromanzanog@gmail.com>
2019-01-29 13:13:45 -0600

    Modificar estilos de mensaje

---
+++ src/index.js
@@ -0,0 +1,16 @@
+class Cipher {
+
+encode(text) {
+    let encodedText = text;
+    return encodedText;
+}
+
+Decode(text) {
+    let decodedText = text;
+    return decodedText;
+ }
+}
+
+let Cipher = new Cipher ();
+console.log(cipher.encode("Hola Carolina"));
+console.log(cipher.decode("Hola Carolina"));
