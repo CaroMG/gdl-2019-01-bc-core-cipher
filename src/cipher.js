396e726efe33bb7783b235c93f84fd7236a96a14
CaroMG <caromanzanog@gmail.com>
2019-01-31 13:23:29 -0600

    metí código en cipher, cambie imagen de fondo, cree un botón nuevo de tipo numero para insertar los datos del offset

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,37 @@
 window.cipher = {
-  // ... 
-};
+
+encode:function  (text,offset) {
+
+  let encryption = "";
+
+  for (let i = 0; i <text.length;i ++) {
+
+      let codeASCII = (text.charCodeAt(i) - 65 + offset) % 26 + 65;
+
+      if (text [i] == " ") encryption = encryption + " ";
+
+      else encryption = encryption + String.fromCharCode(codeASCII);
+
+  }
+
+  return encryption;
+}
+
+decode:function (text,offset) {
+
+  let decryption = "";
+
+  for (let i = 0; i < text.length; i++) {
+
+      let reveal = (text.charCodeAt(i)+ 65 - offset) % 26 + 65;
+
+      if (text[i] == " ") decryption = decryption +" ";
+
+      else decryption = decryption + String.fromCharCode(reveal);
+
+  }
+
+  return decryption;
+
+}
+}
--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
-class Cipher {
+let arr = [];
+	str = str.toUpperCase();
+	for(let i = 0; i < str.length ; i++ ){
 
-encode(text) {
-    let encodedText = text;
-    return encodedText;
-}
+		console.log('charCode ' + str.charCodeAt(i));
+		console.log(String.fromCharCode(str.charCodeAt(i)));
+		//para que el output text respete los espacios dentro del string
+		if (str.charCodeAt(i) === 32) {
+			console.log(String.fromCharCode(32));
+			arr.push(String.fromCharCode(32));
+		}
+		else {
+			console.log(String.fromCharCode(((str.charCodeAt(i) - 65 - offset) % 26 + 65)));
+			arr.push(String.fromCharCode(((str.charCodeAt(i) - 65 - offset) % 26 + 65)));
+			
+		}
 
-Decode(text) {
-    let decodedText = text;
-    return decodedText;
- }
-}
+	}
+ arr = arr.join('');
 
-let Cipher = new Cipher ();
-console.log(cipher.encode("Hola Carolina"));
-console.log(cipher.decode("Hola Carolina"));
+
